c56b7567810ba691788cfb0b62c2588e5e7b01fd
mythicalbadger <jussy12.work@gmail.com>
2023-10-12 16:51:27 +0700

    Added super basic react frontend

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+	  	<img src="https://media.licdn.com/dms/image/D5603AQE_6UZO-RfzzQ/profile-displayphoto-shrink_800_800/0/1669611939830?e=2147483647&v=beta&t=NvZ-plpPmvL7BqlGgCSw9l9c4Q8oVMwiuLQ7w1lwD_Q"></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
