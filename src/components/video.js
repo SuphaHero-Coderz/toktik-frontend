0ab8f22009da699820a2ff00d1779ca6a75fe767
Suphawit <suphawit.won@gmail.com>
2023-10-20 21:38:36 +0700

    added user display in video page

--- src/components/video.js
+++ src/components/video.js
@@ -1,5 +1,8 @@
 import { AspectRatio } from '@chakra-ui/react'
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Square, Grid, GridItem } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react'
+import { Flex,  Stack, Heading, Text,  Card, Avatar ,  CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 
 function Vid() {
   return (
@@ -12,6 +15,27 @@ function Vid() {
                         allowFullScreen
                       />
                     </AspectRatio>
+                </GridItem>
+                <GridItem colSpan={2}>
+                    <Square>     
+                        <Card w = "90%">
+                          <CardBody>
+                            <Flex spacing='4'>
+                              <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+                                <Avatar name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
+
+                                <Box>
+                                  <Heading size='sm'>Segun Adebayo</Heading>
+                                  <Text>Creator, Chakra UI</Text>
+                                </Box>
+                              </Flex>
+                            </Flex>
+                              <Text>
+                                    Kiss me on my hot mouse. 
+                              </Text>
+                          </CardBody>
+                        </Card>
+                    </Square>
                 </GridItem>
           </Grid>
   );
