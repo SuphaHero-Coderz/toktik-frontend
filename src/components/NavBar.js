e4a77e51b8d5fdb1be0240eba768c8db206bbb32
mythicalbadger <jussy12.work@gmail.com>
2023-10-22 21:53:08 +0700

    Added login/register to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css'
 
 const NavBar = () => {
-
-  return (
-    <nav className="navbar">
-      <div className="logo">
-        <img src="/images/toktik-logo.png" alt="TokTik Logo" style={{ height: '40px', marginRight: '10px' }} />
-      </div>
-      <ul className="navList">
-        <li className="navItem"><Link to="/">Home</Link></li>
-        <li className="navItem">
-          <Link to="/upload">Upload</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+	return (
+		<nav className="navbar">
+		<div className="logo">
+			<img src="/images/toktik-logo.png" alt="TokTik Logo" style={{ height: '40px', marginRight: '10px' }} />
+		</div>
+		<ul className="navList">
+			<li className="navItem"><Link to="/">Home</Link></li>
+			<li className="navItem">
+				<Link to="/upload">Upload</Link>
+			</li>
+			<li className="navItem">
+				<Link to="/login">Login</Link>
+			</li>
+			<li className="navItem">
+				<Link to="/register">Register</Link>
+			</li>
+		</ul>
+		</nav>
+	);
 }
 
 export default NavBar;
