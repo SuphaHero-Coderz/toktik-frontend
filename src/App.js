c16e3ced1f35f2123bddf518505f07a76ca776c9
mythicalbadger <jussy12.work@gmail.com>
2023-10-25 08:25:11 +0700

    Redirect to login for unauthenticated pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import VideoPage from './components/VideoPage';
 import HomePage from './components/HomePage';
 import RegisterPage from './components/RegisterPage';
 import UserVideosPage from './components/UserVideosPage';
+import { PrivateRoute } from './components/PrivateRoute';
+
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react'
 
@@ -15,12 +17,12 @@ function App() {
 			<Router>
 			<NavBar />
 				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/upload" element={<UploadPage />} />
+					<Route path="/" element={<PrivateRoute><HomePage /></PrivateRoute>} />
+					<Route path="/upload" element={<PrivateRoute><UploadPage /></PrivateRoute>} />
 					<Route path="/login" element={<LoginPage />} />
 					<Route path="/register" element={<RegisterPage />} />
-					<Route path="/video" element={<VideoPage />} />
-					<Route path="/my-videos" element={<UserVideosPage/>} />
+					<Route path="/video" element={<PrivateRoute><VideoPage /></PrivateRoute>} />
+					<Route path="/my-videos" element={<PrivateRoute><UserVideosPage/></PrivateRoute>} />
 				</Routes>
 			</Router>
 		</ChakraProvider>
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { Route, Navigate, useLocation } from 'react-router-dom';
+export { PrivateRoute };
+
+function PrivateRoute({ children }: { children: JSX.Element }) {
+	const [token, _] = useState(localStorage.getItem("awesometoken"));
+	const location = useLocation();
+
+	if (!token) {
+		return <Navigate to="/login" state={{ from: location }} />;
+	}
+
+	return children;
+}
