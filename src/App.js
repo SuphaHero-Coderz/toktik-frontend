ae1c3db163743d1503a6bc818610439cb317ac04
mythicalbadger <jussy12.work@gmail.com>
2023-10-26 22:25:10 +0700

    No more NavBar on video page

--- src/App.js
+++ src/App.js
@@ -8,22 +8,30 @@ import RegisterPage from './components/RegisterPage';
 import UserVideosPage from './components/UserVideosPage';
 import NotFoundPage from './components/NotFoundPage';
 import { PrivateRoute } from './components/PrivateRoute';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, Outlet } from 'react-router-dom';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const NavBarLayout = () => (
+  <>
+    <NavBar />
+    <Outlet />
+  </>
+);
 
 function App() {
 	return (
 		<ChakraProvider>
 			<Router>
-			<NavBar />
 				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/upload" element={<PrivateRoute><UploadPage /></PrivateRoute>} />
-					<Route path="/login" element={<LoginPage />} />
-					<Route path="/register" element={<RegisterPage />} />
+					<Route element={<NavBarLayout />}>
+						<Route path="/" element={<HomePage />} />
+						<Route path="/upload" element={<PrivateRoute><UploadPage /></PrivateRoute>} />
+						<Route path="/login" element={<LoginPage />} />
+						<Route path="/register" element={<RegisterPage />} />
+						<Route path="/my-videos" element={<PrivateRoute><UserVideosPage/></PrivateRoute>} />
+						<Route path='*' element={<NotFoundPage />}/>
+					</Route>
 					<Route path="/video" element={<PrivateRoute><VideoPage /></PrivateRoute>} />
-					<Route path="/my-videos" element={<PrivateRoute><UserVideosPage/></PrivateRoute>} />
-					<Route path='*' element={<NotFoundPage />}/>
 				</Routes>
 			</Router>
 		</ChakraProvider>
