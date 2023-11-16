3ef4a109c1dbe3c0526c49f0bbcc8bef0456a02a
Suphawit <suphawit.won@gmail.com>
2023-11-16 20:05:00 +0700

    meh

--- src/components/socket.js
+++ src/components/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client"
 
-const socket = io("http://socket:3001", { autoConnect: false });
+const socket = io("http://localhost:3001", { transports: ['websocket'], autoConnect: false });
 
 export default socket;
